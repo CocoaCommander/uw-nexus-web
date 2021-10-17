6b93b2b7a203803764f40b2bb3ffa0b9ec5fa23f
Ryan Lee <ralee478.school@gmail.com>
2021-10-17 16:07:49 -0700

    initial text and images for preview page implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import logo from "./assets/Logo.png";
+import visPanel from "./assets/visPanel.svg";
+import twoPpl from "./assets/tw.png"
 import './App.css';
 
-function App() {
+const TwoPplWrapper = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <img src={twoPpl} aria-hidden="true"/>
+  );
+}
+
+const App = () => {
+  return (
+    <div>
+      <div className={"logoContainer"}>
+        <img src={logo} alt={"nexus logo"}/>
+      </div>
+      <h1>Coming soon to UW students!</h1>
+      <div className={"container"}>
+        <TwoPplWrapper />
+        <p className={"bodyText"}>At the University of Washington, those who are unable to find internships still need to find resume-worthy experiences to remain competitive with their peers. By joining the NEXUS network, you will be able to find intriguing projects as a team member or motivated students as a project manager. We look forward to helping the UW student community and make sure to check out some of our designs below!</p>
+        <TwoPplWrapper />
+      </div>
+      <h1>Get notified when we launch</h1>
+      {/* email submission */}
+      <img src={visPanel} aria-hidden={"true"} className={"visualPanel"}/>
+      <h1>Check out our designs!</h1>
     </div>
   );
 }
