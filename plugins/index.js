17b60b194fb2dec05f20f1c3e20d6cea0e45d800
gboksm11 <gboksm11@uw.edu>
2022-08-09 22:23:26 -0700

    fixed plugins

--- plugins/index.js
+++ plugins/index.js
@@ -1,9 +1,10 @@
-export const onPreBuild = function({ netlifyConfig }) {
+module.exports = {
+  onPreBuild({ netlifyConfig }) {
 
-  netlifyConfig.redirects.push({
-    from: "/api/*",
-    to: "https://uw-nexus-backend.herokuapp.com/api/:splat",
-    status: 200
-  });
-
-}
+    netlifyConfig.redirects.push({
+      from: "/api/*",
+      to: "https://uw-nexus-backend.herokuapp.com/api/:splat",
+      status: 200
+    });
+  },
+};
