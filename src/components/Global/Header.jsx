5e0829131f8f6a81247a5eea9214984f2438cc19
jcorille-21 <jcorille@uw.edu>
2023-03-29 18:24:32 -0700

    added projects dropdown to mobile nav

--- src/components/Global/Header.jsx
+++ src/components/Global/Header.jsx
@@ -9,6 +9,7 @@ import { useEffect, useState, useRef } from 'react';
 import ProfileModal from './ProfileModal.jsx';
 import CreateProjectHeader from "./CreateProjectHeader.jsx";
 import menuIcon from '../../assets/menu-icon.png';
+import dropdownIcon from '../../assets/dropdown-arrow.png';
 
 const LoginButton = ({
     isMobile
@@ -20,12 +21,12 @@ const LoginButton = ({
     const handleClick = async () => {
         if (isLoggedIn) {
             const options = {
-              method: 'DELETE'
+                method: 'DELETE'
             }
             await fetch(SIGN_OUT, options);
             console.log("removing cookie!");
             const prevCookie = new Cookies();
-            prevCookie.remove("fr-accessToken", {path : '/'});
+            prevCookie.remove("fr-accessToken", { path: '/' });
             window.localStorage.removeItem("nxs-id");
             // prevCookie.remove("nxs_id");
             dispatch(setLoggedIn(false));
@@ -49,14 +50,15 @@ const Header = ({
 }) => {
     const [isMenuOpen, setMenuOpen] = useState(false);
     const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const [isProjDropdownClicked, setProjDropdownClicked] = useState(false);
     const menuRef = useRef();
     const location = useLocation().pathname;
     const getUserProfile = () => {
-      getProfile();
+        getProfile();
     }
 
     const isSignUpFlow = location == '/createProfile' || location == '/createProfileStart';
-    
+
     // Set up clicking event for dropdown user accounts and modals
     useEffect(() => {
         const checkIfClickedOutside = e => {
@@ -85,7 +87,7 @@ const Header = ({
             setMobileMenuOpen(false);
         }
 
-        if (location === '/createProfile' || location === '/createProject' || location === '/finishProject' 
+        if (location === '/createProfile' || location === '/createProject' || location === '/finishProject'
             || location == '/reviewProject' || location == '/createProfileStart' || location == '/welcomePage'
             || location == '/login' || location == '/signUp') {
             let maxSteps = 4;
@@ -134,12 +136,34 @@ const Header = ({
                         <div className="side-menu-container">
                             <Link className={location === '/projects' ? 'side-menu-option-active' : 'side-menu-option'} to={"/projects"} onClick={closeMobileMenu}>Discover Projects</Link>
                             {isLoggedIn && <Link className={location === '/profile' ? 'side-menu-option-active' : 'side-menu-option'} to='/profile' onClick={closeMobileMenu}>My Profile</Link>}
+                            <div className="projects-dropdown-container" onClick={() => setProjDropdownClicked(!isProjDropdownClicked)}>
+                                <p className="projects-dropdown">Projects</p>
+                                {isProjDropdownClicked ?
+                                    <p className="projects-dropdown">&or;</p> :
+                                    <p className="projects-dropdown">&#62;</p>
+                                }
+                            </div>
+                            {isProjDropdownClicked ?
+                                <div className="projects-filter-container">
+                                    <input className="projects-filter-button" type="radio" id="all" name="project-filter" value="All" checked />
+                                    <label className="projects-filter" htmlFor="all">All</label>
+                                    <input className="projects-filter-button" type="radio" id="current" name="project-filter" value="Current" />
+                                    <label className="projects-filter" htmlFor="current">Current</label>
+                                    <input className="projects-filter-button" type="radio" id="accepted" name="project-filter" value="Accepted" />
+                                    <label className="projects-filter" htmlFor="accepted">Accepted</label>
+                                    <input className="projects-filter-button" type="radio" id="pending" name="project-filter" value="Pending" />
+                                    <label className="projects-filter" htmlFor="pending">Pending</label>
+                                    <input className="projects-filter-button" type="radio" id="declined" name="project-filter" value="Declined" />
+                                    <label className="projects-filter" htmlFor="declined">Declined</label>
+                                </div> :
+                                null
+                            }
                             <LoginButton isMobile={isMobile} />
                             {isLoggedIn ?
                                 <CreateProjectHeader className="header-create-project" /> :
                                 null}
                         </div>
-    
+
                     </Menu>
                     <div className="flex">
                         <Link to={"/"}>
@@ -165,7 +189,7 @@ const Header = ({
                     {!isSignUpFlow && <NavLink className="projects-button" to={"/projects"}>Discover Projects</NavLink>}
                     {/* {isLoggedIn ? <p className="profile-button" onClick={() => setMenuOpen(true)}>My Profile</p>: null} */}
                     {isLoggedIn && !isSignUpFlow && <p className={location === '/profile' ? 'profile-button-active' : 'profile-button'} onClick={() => setMenuOpen(true)}>My Profile</p>}
-                    {isMenuOpen ? <ProfileModal userProfile={userProfile} menuRef={menuRef} menuCallback={setMenuOpen} getUserProfile={getUserProfile}/> : null}
+                    {isMenuOpen ? <ProfileModal userProfile={userProfile} menuRef={menuRef} menuCallback={setMenuOpen} getUserProfile={getUserProfile} /> : null}
                     <LoginButton />
                 </div>
             </div>
